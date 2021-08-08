499e815b8be9c138873acc716063b52875cb8205
NAGA SIVA POLURI <npoluri1@gmail.com>
2021-08-08 11:27:36 +0800

    2nd commit

--- src/main/java/com/naga/consul/api/SpringJenkinsApplication.java
+++ src/main/java/com/naga/consul/api/SpringJenkinsApplication.java
@@ -2,13 +2,16 @@ package com.naga.consul.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringJenkinsApplication {
 
 	Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 	
-	@postConstruct
+	@PostConstruct
 	public void init() {
 		logger.info("Application started...");
 	}
--- src/test/java/com/naga/consul/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/naga/consul/api/SpringJenkinsApplicationTests.java
@@ -1,6 +1,8 @@
 package com.naga.consul.api;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
